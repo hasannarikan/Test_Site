679ff2cbe3849d8155ecc3035e64b85c51e75ea1
Crekix <66575044+Crekix@users.noreply.github.com>
2020-11-25 01:04:26 +0300

    Update Rectangle.c

--- Rectangle.c
+++ Rectangle.c
@@ -4,31 +4,37 @@
 //Rectangle
 int main(void)
 {
-	unsigned int n;
-	printf_s("%s", "Please enter the value n: ");
-	scanf_s("%u", &n);
-	char star = '*';
-	char empty[200] = " ";
-	for (unsigned int k = 1; k <= n-3; k++)
+	while (true)
 	{
-		strcat_s(empty, " ");
-	}
-	
+		unsigned int n;
+		printf_s("%s", "Please enter the value n (n>2): ");
+		scanf_s("%u", &n);
+		if (n < 2) { printf("\nYou can't define n lesser than 2\n"); return false; }
+		char star = '*';
+		char empty[200] = " ";
+		for (unsigned int k = 1; k <= n - 3; k++)
+		{
+			strcat_s(empty, " ");
+		}
 
-	for (unsigned int i = 1; i <= n; i++)
-	{
-		if (i == 1 || i == n )
+
+		for (unsigned int i = 1; i <= n; i++)
 		{
-			printf("\n");
-			for (unsigned int j = 1; j <= n; j++)
+			if (i == 1 || i == n)
 			{
-				printf("%c", star);
+				printf("\n");
+				for (unsigned int j = 1; j <= n; j++)
+				{
+					printf("%c", star);
+				}
+
 			}
-			
+			else
+			{
+				printf("\n%c%s%c", star, empty, star);
+			}
 		}
-		else
-		{
-			printf("\n%c%s%c", star, empty, star);
-		}
+		printf_s("\nWrite 0 to close program.\n");
 	}
 	return 0;
+}
